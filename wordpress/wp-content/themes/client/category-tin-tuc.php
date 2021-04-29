03dad4f190eefeb02c97d50b349464edc0245ce5
chienhd <chienhd>
2021-04-29 23:24:43 +0700

    post

--- wordpress/wp-content/themes/client/category-tin-tuc.php
+++ wordpress/wp-content/themes/client/category-tin-tuc.php
@@ -12,96 +12,103 @@ get_header();
 $news = get_category_by_slug('tin-tuc');
 
 $categories = get_categories(
-    array( 'parent' => $news->cat_ID )
+    array('parent' => $news->cat_ID)
 );
 
 ?>
 
-<main id="category" class="site-category-news">
-    <div class="title">
-        <h1 class="category-title"><?php echo $news->name; ?></h1>
-    </div>
-    <div class="category-tab">
-        <ul id="category-tab_top" class="nav nav-tabs" role="tablist">
-            <?php 
-            if($categories) { 
+    <main id="category" class="site-category-news">
+        <div class="title">
+            <h1 class="category-title"><?php echo $news->name; ?></h1>
+        </div>
+        <div class="category-tab">
+            <ul id="category-tab_top" class="nav nav-tabs" role="tablist">
+                <?php
+                if ($categories) {
                 foreach ($categories as $key => $value) {
-                ?>
-                    <li role="presentation" class="<?php if($key == 0){ echo 'active'; } ?>">
-                        <a href="#category-tab-<?php echo $key; ?>" aria-controls="category-tab-<?php echo $key; ?>" role="tab"
-                        data-toggle="tab" aria-expanded="true"><?php echo $value->name; ?></a></li>
+                    ?>
+                    <li role="presentation" class="<?php if ($key == 0) {
+                        echo 'active';
+                    } ?>">
+                        <a href="#category-tab-<?php echo $key; ?>" aria-controls="category-tab-<?php echo $key; ?>"
+                           role="tab"
+                           data-toggle="tab" aria-expanded="true"><?php echo $value->name; ?></a></li>
                 <?php } ?>
             </ul>
 
             <div class="tab-content wrap-project-content">
                 <?php foreach ($categories as $key => $value) { ?>
-                <div role="tabpanel" class="tab-pane in <?php if($key == 0){ echo 'active'; } ?>" id="category-tab-<?php echo $key; ?>">
-                    <div class="container container--mod">
-                        <div class="row">
-                            <?php
-                             $query = new WP_Query(
-                                array(
-                                    'post_type' => 'post',
-                                    'post_status' => 'publish',
-                                    'order' => 'DESC',
-                                    'orderby' => 'ID',
-                                    'cat' => $value->cat_ID,
-                                    'posts_per_page' => 6
-                                )
-                            );
-                            $i = 1;
-                            while ($query->have_posts()) {
-                                $query->the_post();
-                               ?>
-                                <div class="col-md-4 col-sm-6">
-                                    <div class="project-content-item">
-                                        <a href="<?php echo esc_url(get_permalink()) ?>">
-                                            <div class="project-content-item_top">
-                                                <div class="image">
+                    <div role="tabpanel" class="tab-pane in <?php if ($key == 0) {
+                        echo 'active';
+                    } ?>" id="category-tab-<?php echo $key; ?>">
+                        <div class="container container--mod">
+                            <div class="row">
+                                <?php
+                                $query = new WP_Query(
+                                    array(
+                                        'post_type' => 'post',
+                                        'post_status' => 'publish',
+                                        'order' => 'DESC',
+                                        'orderby' => 'ID',
+                                        'cat' => $value->cat_ID,
+                                        'posts_per_page' => 6
+                                    )
+                                );
+                                $i = 1;
+                                while ($query->have_posts()) {
+                                    $query->the_post();
+                                    ?>
+                                    <div class="col-md-4 col-sm-6">
+                                        <div class="project-content-item">
+                                            <a href="<?php echo esc_url(get_permalink()) ?>">
+                                                <div class="project-content-item_top">
+                                                    <div class="image">
 
-                                                    <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php echo get_the_title(); ?>">
-                                                </div>
-                                                <div class="info">
-                                                    <div class="name"><?php echo get_the_title(); ?>
+                                                        <img src="<?php echo get_the_post_thumbnail_url(); ?>"
+                                                             alt="<?php echo get_the_title(); ?>">
+                                                    </div>
+                                                    <div class="info">
+                                                        <div class="name"><?php echo get_the_title(); ?>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div class="project-content-item_bottom clearfix">
-                                                <div class="number">0<?php echo $i; ?></div>
-                                                <div class="subtitle-wrap">
-                                                    <h3 class="subtitle"><?php echo get_the_title(); ?></h3>
+                                                <div class="project-content-item_bottom clearfix">
+                                                    <div class="number">0<?php echo $i; ?></div>
+                                                    <div class="subtitle-wrap">
+                                                        <h3 class="subtitle"><?php echo get_the_title(); ?></h3>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </a>
+                                            </a>
+                                        </div>
                                     </div>
-                                </div>
-                               <?php
+                                    <?php
 
-                               $i++;
-                        }
-                            wp_reset_postdata(); 
-                            ?>
+                                    $i++;
+                                }
+                                wp_reset_postdata();
+                                ?>
+                            </div>
                         </div>
                     </div>
-                </div>
-                 <?php } ?>
-     <!-- link here -->
+                <?php } ?>
+                <!-- link here -->
                 <div class="container-fluid">
                     <div class="col-xs-12 ">
-                 <a class="home-project-view-more hvr-grow" href="<?php echo $value->slug; ?>">Xem thêm&gt;&gt; "<?php echo $value->name ?>"</a>
-             </div>
-                 </div>
+                        <a class="home-project-view-more hvr-grow" href="<?php echo $value->slug; ?>">Xem thêm&gt;&gt;
+                            "<?php echo $value->name ?>"</a>
+                    </div>
+                </div>
             </div>
 
             <?php
             }
             ?>
-    </div>
+        </div>
 
 
-    <?php get_template_part('template-parts/home', 'choose-us'); ?>
+        <?php get_template_part('template-parts/home', 'choose-us'); ?>
 
-</main><!-- #main -->
+    </main><!-- #main -->
 
 <?php
 get_footer();
--- wordpress/wp-content/themes/client/template-parts/content-single.php
+++ wordpress/wp-content/themes/client/template-parts/content-single.php
@@ -11,109 +11,76 @@
 <div class="container">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 main-content main-content-single">
         <div id="single-title">
-        <h2><?php echo get_the_title(); ?></h2>
+            <h2><?php echo get_the_title(); ?></h2>
         </div>
         <div id="single-content" class="single-content">
             <?php
             the_content(
                 sprintf(
                     wp_kses(
-                        /* translators: %s: Name of current post. Only visible to screen readers */
-                        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'client' ),
+                    /* translators: %s: Name of current post. Only visible to screen readers */
+                        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'client'),
                         array(
                             'span' => array(
                                 'class' => array(),
                             ),
                         )
                     ),
-                    wp_kses_post( get_the_title() )
+                    wp_kses_post(get_the_title())
                 )
             );
 
             wp_link_pages(
                 array(
-                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'client' ),
-                    'after'  => '</div>',
+                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'client'),
+                    'after' => '</div>',
                 )
             );
             ?>
         </div><!-- .entry-content -->
 
         <div class="single-single_readmore">
-            <p><?php echo get_next_post_link('<span>>> Xem thêm: %link</span>');?></p>
+            <p><?php echo get_next_post_link('<span>>> Xem thêm: %link</span>'); ?></p>
             <p><?php echo get_previous_post_link('<span>>> Xem thêm: %link<span>'); ?></p>
         </div>
 
         <h3 class="section-title">Có thể bạn quan tâm</h3>
         <div class="single-related-loop row">
-           <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 related-item1">
-                 <div class="ih-item square effect6 from_top_and_bottom">
-                    <a href="https://byzan.vn/mau-sac-cua-nam-2021/">
-                       <div class="img"> <img src="https://byzan.vn/wp-content/uploads/2020/12/mau-sac-cua-nam-2021-2.jpg" alt="img"></div>
-                       <div class="info">
-                          <h3>XÁM TRUNG TÍNH LÀ 1 TRONG 2 MÀU SẮC CỦA NĂM 2021</h3>
-                       </div>
-                    </a>
-                 </div>
-                 <a class="related-item-title" href="https://byzan.vn/mau-sac-cua-nam-2021/"><span>XÁM TRUNG TÍNH LÀ 1 TRONG 2 MÀU SẮC CỦA NĂM 2021</span></a>
-              </div>
-              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 related-item2">
-                 <div class="ih-item square effect6 from_top_and_bottom">
-                    <a href="https://byzan.vn/xu-huong-thiet-ke-noi-that/">
-                       <div class="img"> <img src="https://byzan.vn/wp-content/uploads/2020/10/1cce1e103156055.5f46b4b0bfadb.jpg" alt="img"></div>
-                       <div class="info">
-                          <h3>8 XU HƯỚNG THIẾT KẾ NỘI THẤT PHỔ BIẾN NĂM 2020</h3>
-                       </div>
-                    </a>
-                 </div>
-                 <a class="related-item-title" href="https://byzan.vn/xu-huong-thiet-ke-noi-that/"><span>8 XU HƯỚNG THIẾT KẾ NỘI THẤT PHỔ BIẾN NĂM 2020</span></a>
-              </div>
-              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 related-item3">
-                 <div class="ih-item square effect6 from_top_and_bottom">
-                    <a href="https://byzan.vn/phong-cach-noi-that-dan-dau-xu-huong-nam-2020/">
-                       <div class="img"> <img src="https://byzan.vn/wp-content/uploads/2020/07/3-2.jpg" alt="img"></div>
-                       <div class="info">
-                          <h3>TÌM HIỂU PHONG CÁCH NỘI THẤT DẪN ĐẦU XU HƯỚNG NĂM 2020</h3>
-                       </div>
-                    </a>
-                 </div>
-                 <a class="related-item-title" href="https://byzan.vn/phong-cach-noi-that-dan-dau-xu-huong-nam-2020/"><span>TÌM HIỂU PHONG CÁCH NỘI THẤT DẪN ĐẦU XU HƯỚNG NĂM 2020</span></a>
-              </div>
-              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 related-item4">
-                 <div class="ih-item square effect6 from_top_and_bottom">
-                    <a href="https://byzan.vn/xu-huong-noi-that-2020/">
-                       <div class="img"> <img src="https://byzan.vn/wp-content/uploads/2020/05/1.1.jpg" alt="img"></div>
-                       <div class="info">
-                          <h3>PHONG CÁCH HIỆN ĐẠI, SANG TRỌNG – ĐÁNH DẤU XU HƯỚNG NỘI THẤT NĂM 2020</h3>
-                       </div>
-                    </a>
-                 </div>
-                 <a class="related-item-title" href="https://byzan.vn/xu-huong-noi-that-2020/"><span>PHONG CÁCH HIỆN ĐẠI, SANG TRỌNG – ĐÁNH DẤU XU HƯỚNG NỘI THẤT NĂM 2020</span></a>
-              </div>
-              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 related-item5">
-                 <div class="ih-item square effect6 from_top_and_bottom">
-                    <a href="https://byzan.vn/10-mau-thiet-ke-noi-that-hien-dai-sang-trong/">
-                       <div class="img"> <img src="https://byzan.vn/wp-content/uploads/2020/05/11-4-scaled.jpg" alt="img"></div>
-                       <div class="info">
-                          <h3>10+ MẪU THIẾT KẾ NỘI THẤT HIỆN ĐẠI, SANG TRỌNG CHO CHUNG CƯ, BIỆT THỰ VÀ NHÀ PHỐ NĂM 2020</h3>
-                       </div>
-                    </a>
-                 </div>
-                 <a class="related-item-title" href="https://byzan.vn/10-mau-thiet-ke-noi-that-hien-dai-sang-trong/"><span>10+ MẪU THIẾT KẾ NỘI THẤT HIỆN ĐẠI, SANG TRỌNG CHO CHUNG CƯ, BIỆT THỰ VÀ NHÀ PHỐ NĂM 2020</span></a>
-              </div>
-              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 related-item6">
-                 <div class="ih-item square effect6 from_top_and_bottom">
-                    <a href="https://byzan.vn/xu-hong-thiet-ke-noi-that-phong-ngu/">
-                       <div class="img"> <img src="https://byzan.vn/wp-content/uploads/2020/04/8d61c641acc04a9e13d1.jpg" alt="img"></div>
-                       <div class="info">
-                          <h3>XU HƯỚNG THIẾT KẾ NỘI THẤT PHÒNG NGỦ ẤN TƯỢNG 2020</h3>
-                       </div>
-                    </a>
-                 </div>
-                 <a class="related-item-title" href="https://byzan.vn/xu-hong-thiet-ke-noi-that-phong-ngu/"><span>XU HƯỚNG THIẾT KẾ NỘI THẤT PHÒNG NGỦ ẤN TƯỢNG 2020</span></a>
-              </div>
-           </div>
+                <?php
+                $post_id = get_the_ID();
+                $current_post_type = get_post_type($post_id);
+                $cat = wp_get_post_categories($post_id);
+                $cat_id = $cat[0];
+                $query_args = array(
+                    'category__in' => $cat_id,
+                    'post_type' => $current_post_type,
+                    'post__not_in' => array($post_id),
+                    'posts_per_page' => '6',
+                );
+
+                $related_cats_post = new WP_Query($query_args);
+                $i = 1;
+                while ($related_cats_post->have_posts()) :
+                    $related_cats_post->the_post();
+                    ?>
+                    <div class="col-md-4 col-sm-6 col-xs-12 related-item<?php echo $i; ?>">
+                        <div class="ih-item square effect6 from_top_and_bottom">
+                            <a href="<?php echo get_the_permalink(); ?>">
+                                <div class="img"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="img"></div>
+                                <div class="info">
+                                    <h3><?php echo get_the_title(); ?></h3>
+                                </div>
+                            </a>
+                        </div>
+                        <a class="related-item-title"
+                           href="<?php echo get_the_permalink(); ?>"><span><?php echo get_the_title(); ?></span></a>
+                    </div>
+                    <?php
+                    $i++;
+                endwhile; // End of the loop.
+                wp_reset_postdata();
+                ?>
         </div>
+    </div>
     </div>
 </div>
